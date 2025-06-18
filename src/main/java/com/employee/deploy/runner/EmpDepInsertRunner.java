29120792ba865b98689bc193f7d6e8aaab3e066d
yj-hong96 <yj.hong960906@gmail.com>
2025-06-18 16:46:24 +0900

    properties update

--- src/main/java/com/employee/deploy/runner/EmpDepInsertRunner.java
+++ src/main/java/com/employee/deploy/runner/EmpDepInsertRunner.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-@Profile("prod")
+@Profile("local")
 public class EmpDepInsertRunner implements ApplicationRunner {
     @Autowired
     DepartmentRepository departmentRepository;
